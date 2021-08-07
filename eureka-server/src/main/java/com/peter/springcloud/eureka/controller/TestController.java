0f0448532e3e6b83514eccb2bed412d6d52d7006
chensong <chensong@jztey.com>
2021-08-07 10:24:13 +0800

    commit

--- eureka-server/src/main/java/com/peter/springcloud/eureka/controller/TestController.java
+++ eureka-server/src/main/java/com/peter/springcloud/eureka/controller/TestController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    private static int i = 0;
     @RequestMapping(value="/test")
-    public Object test(){
+    public Object test()throws Exception{
+        System.out.println(i++);
+        Thread.sleep(10*60*1000);
         return "ok";
     }
 
--- merchandise-server-imp/src/main/java/com/peter/springcloud/merchandise/imp/config/service/NacosConfigServiceImpl.java
+++ merchandise-server-imp/src/main/java/com/peter/springcloud/merchandise/imp/config/service/NacosConfigServiceImpl.java
@@ -2,15 +2,19 @@ package com.peter.springcloud.merchandise.imp.config.service;
 
 import com.peter.springcloud.merchandise.api.config.NacosConfigService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class NacosConfigServiceImpl implements NacosConfigService {
     @Value("${common.name}")
     private String nacosValue;
+    @Value("${infr.common.name}")
+    private String nacosOriginValue;
 
     @Override
     public String getOne() {
-        return nacosValue;
+        return nacosOriginValue;
     }
 }
